d27e19889e12a16c40864cc4f4d84faebc28383f
stefan <stefan.holmberg@systementor.se>
2024-11-06 14:37:42 +0100

    a

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base:'https://aspcodenet.github.io/angular1104/'
+    base:'/angular1104/'
 })
