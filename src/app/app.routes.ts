46df4364ecdc220496cc772db961ac8295cc9cc4
stefan <stefan.holmberg@systementor.se>
2024-11-06 08:45:35 +0100

    forms

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,17 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [
     {
         path:'',
         component:HomeComponent
     },
+    {
+        path:'contact',
+        component:ContactComponent
+    },
+
     // {
     //     path:'products/:id',
     //     component:SingleproductComponent
---
+++ src/app/contact/contact.component.ts
@@ -0,0 +1,34 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-contact',
+  standalone: true,
+  imports: [ ReactiveFormsModule, CommonModule],
+  templateUrl: './contact.component.html',
+  styleUrl: './contact.component.css'
+})
+export class ContactComponent {
+  userForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.userForm = this.formBuilder.group({
+      firstName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(50)]],
+      lastName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(50)]],
+      email: ['', [Validators.required, Validators.email]],
+      txt: ['', Validators.required],
+    });
+  }
+
+  submitForm(): void {
+    if (this.userForm?.valid) {
+      console.log('Form data:', this.userForm.value);
+    }else{
+      console.log('ERROR');
+    }
+  }
+
+}
